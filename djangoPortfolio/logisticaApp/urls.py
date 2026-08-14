9c8e2f4e752c26894cbe56a1fdc85c79933cd6fd
n0t-f0ur13r <xmenmagnet@gmail.com>
2026-08-14 12:47:52 -0400

    Remove empty app

--- djangoPortfolio/logisticaApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-urlpatterns = [
-    path("", views.MainViews.as_view(), name="main"),
-]
--- djangoPortfolio/logisticaApp/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class MainViews(TemplateView):
-    template_name = "logisticaApp/index.html"
